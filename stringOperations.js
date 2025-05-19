34e19ec2577fba9ce750d6768fd882bc1bb0b130
omkorde.rs652 <94031293+Ravsaheb1968@users.noreply.github.com>
2025-05-19 17:41:50 +0000

    Add String Operations

---
+++ stringOperations.js
@@ -0,0 +1,9 @@
+let mystr="  om korde  ";
+console.log(mystr.toUpperCase());
+console.log(mystr.charAt(3));
+console.log(mystr.length);
+console.log(mystr.substring(1,4));
+console.log(mystr.indexOf('o'));
+console.log(mystr.includes('k'));
+console.log(mystr.replace('k','K'))
+console.log(mystr.trim());
