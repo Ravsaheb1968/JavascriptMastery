8573b2d402b2bfce3502ea28089e3b5332f2f7b7
omkorde.rs652 <94031293+Ravsaheb1968@users.noreply.github.com>
2025-05-19 17:48:47 +0000

    add array file operation

---
+++ Array.js
@@ -0,0 +1,10 @@
+let myarr=[24,67,98,90,23,56];
+
+// Push to Add at the End of the Array
+myarr.push(99);
+console.log(myarr)
+
+// Pop to remove the last element of the Array
+myarr.pop(78);
+console.log(myarr)
+
