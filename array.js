f233a09b8048e640b277fda7c1161a874cce6e3d
omkorde.rs652 <94031293+Ravsaheb1968@users.noreply.github.com>
2025-05-19 18:15:57 +0000

    add array file

---
+++ array.js
@@ -0,0 +1,30 @@
+let myarr=[34,64,87,12,54]
+console.log(myarr);
+
+// add the Element at the end of arr
+console.log(myarr.push(24));
+console.log(myarr);
+
+// Remove the Element from the end of arr
+console.log(myarr.pop(24));
+console.log(myarr);
+
+// add Element starting of arr
+console.log(myarr.unshift(652));
+console.log(myarr);
+
+// remove element from starting of arr
+console.log(myarr.shift(652));
+console.log(myarr);
+
+// includes check element present or not in the arr
+console.log(myarr.includes(34));
+
+// Indexoff => find the index of the given Element
+console.log(myarr.indexOf(87))
+
+// Join in the Array
+
+let arr2=myarr.join();
+
+console.log(arr2);
