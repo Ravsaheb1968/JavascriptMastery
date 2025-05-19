b0961f75413e43d9592644608878fe1351bd6113
omkorde.rs652 <94031293+Ravsaheb1968@users.noreply.github.com>
2025-05-19 17:09:23 +0000

    Add File DataType_Conversion

---
+++ DataType_Conversion.js
@@ -0,0 +1,25 @@
+// Data Types in the Js
+// 1. Number
+// 2. String
+// 3. Boolean
+// 4. Undefined
+// 5. Null
+// 6. Symbol
+// 7. BigInt
+
+console.log(typeof undefined);
+console.log(typeof null);
+
+// Datatype Conversion
+
+let alphanum = "33abc";
+let strNum="652";
+let nullNum= null
+let undefinedNum=undefined;
+
+console.log(Number(alphanum));
+console.log(Number(nullNum));
+console.log(Number(undefinedNum));
+console.log(Number(strNum));
+
+
