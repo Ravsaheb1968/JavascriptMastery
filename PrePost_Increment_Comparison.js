7f9aaa14ae9defe62acb2df8c9dcca3b45376c5b
omkorde.rs652 <94031293+Ravsaheb1968@users.noreply.github.com>
2025-05-19 17:20:21 +0000

    Add file PrePost Increment_Comparison

---
+++ PrePost_Increment_Comparison.js
@@ -0,0 +1,24 @@
+let a=5;
+let b=++a;
+console.log(a,b);
+// output a=6, b=6
+
+let x=5;
+let y=x++
+console.log(x,y);
+
+//output x=6 y=5; 
+
+// Comparison 
+
+console.log(null==undefined);
+// o/p => True
+
+console.log(null==0);
+// o/p => False
+
+console.log("2"==2);
+// o/p => True
+
+console.log("2"===2);
+// o/p => False
