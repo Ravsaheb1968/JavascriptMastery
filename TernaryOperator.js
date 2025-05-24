364d40844470eeb700b98f9da441f41f697c9d6e
omkorde.rs652 <94031293+Ravsaheb1968@users.noreply.github.com>
2025-05-24 18:57:05 +0000

    Add File Ternary Operator

---
+++ TernaryOperator.js
@@ -0,0 +1,13 @@
+let a=20;
+a % 2==0 ? console.log("a is greater than 20") : console.log("a is smaller than 20");
+
+// Nullish Coalescing Operator
+
+let val
+val=3??20;
+console.log(val);
+
+val=null??10;
+console.log(val);
+
+
