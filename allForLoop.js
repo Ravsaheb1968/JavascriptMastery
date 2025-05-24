0be4415ea3b5b9aa0b56455b03b3750a95315e17
omkorde.rs652 <94031293+Ravsaheb1968@users.noreply.github.com>
2025-05-24 19:10:15 +0000

    addForLoop

---
+++ allForLoop.js
@@ -0,0 +1,22 @@
+// Simple for loop
+
+// let i=10;
+// console.log("it's all about writing the 10 table")
+// for(let j=1;j<=10;j++){
+//     console.log(j*2);
+// }
+
+const myarr=[42,24,2442,42,241];
+
+// for in Loop(it gives the key of the array not an value)=> used into the Array 
+
+// for(i in myarr){
+//     console.log(myarr[i]);
+// }
+
+// for of Loop(it gives the value of array not an keys)
+
+for(i of myarr){
+    console.log(i);
+}
+
