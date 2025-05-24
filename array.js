7ecebfd177d63091ce3dfa3e188a4543329ffc05
omkorde.rs652 <94031293+Ravsaheb1968@users.noreply.github.com>
2025-05-24 13:45:57 +0000

    Update with more info

--- array.js
+++ array.js
@@ -28,3 +28,35 @@ console.log(myarr.indexOf(87))
 let arr2=myarr.join();
 
 console.log(arr2);
+
+// Difference between splice and slice
+
+const newarr=myarr.splice(0,3);
+console.log(newarr);
+console.log(myarr);
+
+// Adding Two Array
+
+const arrConcat=myarr.concat(newarr);
+console.log(arrConcat);
+
+// another method to concate an arr
+
+let arr1=[43,35,5,12,55];
+let arr5=[34,53,5,45,522,5,32]
+
+const arr7= [...arr1,...arr5]
+console.log(arr7);
+
+let mixarr=[2,42,4,[242,24,2,2444],[24422,2,24,42,4,[4,2,42,42,22]]]
+
+console.log(mixarr.flat(Infinity));
+
+// it is possible to object to array conversion?
+
+// string to array conversion
+
+str="omkorde";
+
+console.log(Array.from(str));
+
