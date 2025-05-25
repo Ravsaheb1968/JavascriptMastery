d514028d3583bb12b28917b6bcc3137c8bb3c2ff
omkorde.rs652 <94031293+Ravsaheb1968@users.noreply.github.com>
2025-05-25 07:58:51 +0000

    Add file allforLoop method

--- allForLoop.js
+++ allForLoop.js
@@ -8,7 +8,7 @@
 
 const myarr=[42,24,2442,42,241];
 
-// for in Loop(it gives the key of the array not an value)=> used into the Array 
+// for in Loop(it gives the key of the array not an value)=> used into the Object mostly 
 
 // for(i in myarr){
 //     console.log(myarr[i]);
@@ -19,4 +19,46 @@ const myarr=[42,24,2442,42,241];
 for(i of myarr){
     console.log(i);
 }
+
+// for Of loop and for in loop in Object
+
+const objForIn={
+    name:"Om",
+    age:45,
+    id:50
+}
+// for in loop in the object
+for(j in objForIn){
+    console.log(j,objForIn[j])
+}
+
+// for of loop in the object its not possible beacause object never be iterable
+
+// for(k of objForIn){
+//     console.log(k)
+// }
+
+// For Each Loop 
+
+// In the forEach loop one function that able to return value
+
+const forArr=[34,535,225,224,24];
+
+// let forEachLoop=forArr.forEach(function(item){
+//     console.log(item);
+// })
+
+// forEach loop with the Arrow function
+
+
+
+let forEachArrow=forArr.forEach((item)=>{
+    console.log(item);
+})
+
+// forEach loop return nothing
+
+// let forEachArrow=forArr.forEach((item)=>{
+//     return item // it is not possible in the forEachLoop
+// })
 
